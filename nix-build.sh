cfeca64ab5b41113a34b06a6fb110f50c6e9d79a
IceDBorn <github.envenomed@dralias.com>
2023-08-03 07:39:52 +0300

    Add jitsi improvements

--- nix-build.sh
+++ nix-build.sh
@@ -3,5 +3,8 @@
 # Use the default instance config
 cp element-web/config.sample.json element-web/config.json
 
+# Patch jitsi with improvements
+git apply jitsi-improvements.patch
+
 # Enter a bash shell with required dependencies and build the webpage
 nix-shell -p cmake yarn --run 'make web-release'
